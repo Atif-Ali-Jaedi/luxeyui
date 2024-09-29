99fa811d3609c2f9af4bc3298210c91bd5504471
Atif Ali Jaedi <atifalijaedi@gmail.com>
2024-09-29 15:20:57 +0600

    feat(badge, scroll shadow): remade the madge to work with avatar and added offsets in scroll shadow

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,12 +1,11 @@
 /* eslint-disable */
 
-const darkMode = localStorage.getItem("darkMode");
-if (darkMode === "true") {
-    document.documentElement.className = "dark";
-    document.querySelector("#tdm").checked = true;
-}
-document.querySelector("#tdm").addEventListener("click", (e) => {
-    const isChecked = e.target.checked;
-    document.documentElement.className = isChecked ? "dark" : "";
-    localStorage.setItem("darkMode", isChecked);
-});
+document
+    .querySelector(".switch span input[type='checkbox']")
+    .addEventListener(
+        "change",
+        (e) =>
+            (document.documentElement.className = e.target.checked
+                ? "dark"
+                : "light"),
+    );
